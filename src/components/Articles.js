492ed396fd4f3e3ea1c790535005ffde39f754f6
Jiri Bazant <jiri.bazant@memsource.com>
2022-01-29 23:28:16 +0100

    Articles fix

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { FlatList } from 'react-native';
 import { Article } from './Article';
 
-export const Articles = ({ data, deleteArticleByIndex }) => (
+export const Articles = ({ data, deleteArticle }) => (
   <FlatList
     data={data}
     renderItem={({ item: { title, index } }) => (
-      <Article title={title} index={index} deleteArticleByIndex={deleteArticleByIndex} />
+      <Article title={title} index={index} deleteArticleByIndex={deleteArticle} />
     )}
   />
 );
